7995f3c2abb3fbe0aead1da925ed9b9250f6e5cd
leep-frog <gleeper49@gmail.com>
2022-06-29 22:37:57 -0400

    untest

--- sourcecontrol.go
+++ sourcecontrol.go
@@ -18,9 +18,8 @@ func BranchCompletor() command.Completor[string] {
 }
 
 var (
-	nvFlag   = command.BoolFlag("no-verify", 'n', "Whether or not to run pre-commit checks")
-	pushFlag = command.BoolFlag("push", 'p', "Whether or not to push afterwards")
-	// test
+	nvFlag     = command.BoolFlag("no-verify", 'n', "Whether or not to run pre-commit checks")
+	pushFlag   = command.BoolFlag("push", 'p', "Whether or not to push afterwards")
 	messageArg = command.ListArg[string]("MESSAGE", "Commit message", 1, command.UnboundedList)
 	branchArg  = command.Arg[string](
 		"BRANCH",
